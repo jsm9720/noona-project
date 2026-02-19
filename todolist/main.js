8b3f547c8462d4cf04bcb8f1c6ac40aedb8100cc
sm <jsm9720@a.ut.ac.kr>
2026-02-20 01:21:13 +0900

    todo list 4 step

--- todolist/main.js
+++ todolist/main.js
@@ -2,6 +2,9 @@
 // + 버튼 클릭 -> 할일 추가
 // delete 클릭 -> 삭제
 // check 클릭 -> 할일 끝 + 밑줄
+// 1. check 클릭 -> true <-> false
+// 2. true이면 끝난걸로 간주하고 밑줄
+// 3. false이면 안끝난걸로 간주하고 그대로
 // 진행 중 끝남 탭 -> 언더바 이동
 // 각 탭에 맞는 결과 표출
 //
@@ -13,8 +16,12 @@ let taskList = [];
 addButton.addEventListener("click", addTask);
 
 function addTask() {
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+    let task = {
+        id: randomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete: false,
+    }
+    taskList.push(task);
     console.log(taskList);
     render();
 }
@@ -22,15 +29,52 @@ function addTask() {
 function render() {
     let resultHTML = '';
     for (let i=0; i<taskList.length; i++){
-        resultHTML += `<div class="task">
-                    <div>
-                        ${taskList[i]}
+        if (taskList[i].isComplete == true){
+            resultHTML += `<div class="task">
+                    <div class="task-done">
+                        ${taskList[i]["taskContent"]}
                     </div>
-                    <div>
-                        <button>Check</button>
-                        <button>Delete</button>
+                    <div class="task-button">
+                        <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-arrows-rotate"></i></button>
+                        <button onclick="deleteTask()"><i class="fa-solid fa-trash"></i></button>
                     </div>
                 </div>`
+        } else {
+            resultHTML += `<div class="task">
+                    <div>
+                        ${taskList[i]["taskContent"]}
+                    </div>
+                    <div class="task-button">
+                        <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-check"></i></button>
+                        <button onclick="deleteTask('${taskList[i].id}')"><i class="fa-solid fa-trash"></i></button>
+                    </div>
+                </div>`
+        }
+        
     }
     document.getElementById("task-board").innerHTML = resultHTML
+}
+
+function toggleComplete(id) {
+    for (let i=0; i<taskList.length; i++){
+        if (taskList[i].id == id) {
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+    }
+    render();
+}
+
+function deleteTask(id) {
+    for (let i=0; i<taskList.length; i++) {
+        if (taskList[i].id == id) {
+            taskList.splice(i,1);
+            break;
+        }
+    }
+    render();
+}
+
+function randomIDGenerate() {
+    return '_' + Math.random().toString(36).substr(2, 9);
 }
